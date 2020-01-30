61b55c6538ae36691d169bab38250e59f7de77aa
Dawid Heyman <dawid.heyman@kontomatik.com>
2020-01-30 10:58:05 +0100

    Refactored WebPageClient

--- src/main/java/com/dawhey/challenge/client/MilleniumWebPageClient.java
+++ src/main/java/com/dawhey/challenge/client/MilleniumWebPageClient.java
@@ -19,26 +19,18 @@ public class MilleniumWebPageClient {
     private String milleniumBaseUrl;
 
     public Connection.Response getMilleniumWelcomePage() {
-        return execute(Jsoup.connect(milleniumBaseUrl + "osobiste2/Retail/Login/MulticodeRequest")
-                .method(Connection.Method.GET)
-                .userAgent(RequestParams.USER_AGENT)
-                .followRedirects(true));
+        return execute(request(Connection.Method.GET, "osobiste2/Retail/Login/MulticodeRequest"));
     }
 
     public Connection.Response performMultiCodeRequest(MulticodeRequest requestData) {
-        return execute(Jsoup.connect(milleniumBaseUrl + "osobiste2/Retail/Login/MulticodeRequest")
-                .method(Connection.Method.POST)
-                .userAgent(RequestParams.USER_AGENT)
+        return execute(request(Connection.Method.POST, "osobiste2/Retail/Login/MulticodeRequest")
                 .cookies(requestData.cookies)
                 .data("Millekod", String.valueOf(requestData.millekod))
-                .data(RequestParams.VERIFICATION_TOKEN_PARAM, requestData.verificationTokenValue)
-                .followRedirects(true));
+                .data(RequestParams.VERIFICATION_TOKEN_PARAM, requestData.verificationTokenValue));
     }
 
     public Connection.Response performPasswordRequest(PasswordRequest requestData) {
-        return execute(Jsoup.connect(milleniumBaseUrl + "osobiste2/Retail/Login/PasswordOneRequest")
-                .method(Connection.Method.POST)
-                .userAgent(RequestParams.USER_AGENT)
+        return execute(request(Connection.Method.POST, "osobiste2/Retail/Login/PasswordOneRequest")
                 .cookies(requestData.cookies)
                 .data(passwordRequestFormStaticData())
                 .data(requestData.peselFormData)
@@ -46,16 +38,19 @@ public class MilleniumWebPageClient {
                 .data(RequestParams.BOT_DETECTION_TOKEN_PARAM, requestData.botDetectionClientToken)
                 .data(RequestParams.VERIFICATION_TOKEN_PARAM, requestData.requestVerificationToken)
                 .data(RequestParams.LOGIN_CHALLENGE_PARAM, requestData.securityDigitsLoginChallengeToken)
-                .data("SecurityDigitsViewModel.LoginPassword", requestData.getSecurityDigitsPassword())
-                .followRedirects(true));
+                .data("SecurityDigitsViewModel.LoginPassword", requestData.getSecurityDigitsPassword()));
     }
 
     public Connection.Response getAccountListPage(Map<String, String> cookies) {
-        return execute(Jsoup.connect(milleniumBaseUrl + "osobiste2/Accounts/CurrentAccountsList/List")
-                .method(Connection.Method.GET)
+        return execute(request(Connection.Method.GET, "osobiste2/Accounts/CurrentAccountsList/List")
+                .cookies(cookies));
+    }
+
+    private Connection request(Connection.Method method, String endpoint) {
+        return Jsoup.connect(milleniumBaseUrl + endpoint)
+                .method(method)
                 .userAgent(RequestParams.USER_AGENT)
-                .cookies(cookies)
-                .followRedirects(true));
+                .followRedirects(true);
     }
 
     private Connection.Response execute(Connection connection) {
